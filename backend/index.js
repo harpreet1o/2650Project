6e9c719e53e0f919ea2a784c0988df3c2d22aa01
Harpreet Singh <harpreetx01@outlook.com>
2024-07-12 08:42:37 -0700

    setup of simple chess logic with socket setup for backend

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ import http from "http";
 import { Chess } from "chess.js";
 import path from "path";
 import { fileURLToPath } from "url";
+import { log } from "console";
 
 // These lines are needed to properly handle __dirname with ES6 modules
 const __filename = fileURLToPath(import.meta.url);
@@ -26,6 +27,50 @@ app.get("/",(req,res)=>{
 })
 io.on("connection",(socket)=>{
     console.log("connected")
+    if(!players.white){
+        players.white=socket.id;
+        socket.emit("playerRole","w");
+    }
+    else if(!players.black){
+        players.black=socket.id;
+        socket.emit("playerRole","b");
+    }
+    else{
+    socket.emit("spectatorRole");
+    }
+    socket.on("move",(move)=>{
+        try{
+            if(chess.turn()==='w'&& socket.id !==players.white) return;
+            if(chess.turn()==='b'&& socket.id !==players.black) return;
+
+            const result=chess.move(move);
+            if(result){
+                currentPlayer=chess.turn();
+                io.emit("move",move);
+                io.emit("boardState",chess.fen())
+        }
+        else{
+            console.log("Invalid move:",move);
+            socket.emit("InvalidMove",move);
+        }
+    }
+        catch(err){
+          console.log(err);
+          socket.emit("Invalid move:",move);
+            }
+
+        
+    })
+    socket.on("disconnect",()=>{
+        if(socket.id===players.white){
+            delete players.white;
+        }
+        if(socket.id===players.black){
+            delete players.black;
+        }
+        
+        console.log("disconnected");
+    })
 })
 
 server.listen(3000,function(){
