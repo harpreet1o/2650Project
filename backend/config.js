062aeca7e28fe3082d2ae94b9fc7f9a7b1d9c8df
gangasandhu <sr.gangasingh@gmail.com>
2024-08-03 13:57:26 -0700

    changes in main pulled

--- backend/config.js
+++ backend/config.js
@@ -5,9 +5,11 @@ dotenv.config();
 
 const config = {
   database: process.env.DATABASE_PATH,
-  secretKeyJWT: process.env.SECRET_KEY_JWT,
+  secretKeyJWT: 'harganga',
   port: process.env.PORT || 3000,
+  googleClientId: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  corsOrigin: process.env.CORS_ORIGIN,
 };
 
 export default config;
-// sql lite implementation so that in future easier to switch to the sql database
