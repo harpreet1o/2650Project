e0321365adbd81987eb25de2071aadc5eb042420
gangasandhu <sr.gangasingh@gmail.com>
2024-08-03 14:58:37 -0700

    chessboard ui improved

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -1,12 +1,9 @@
 import React from 'react';
 import UserProfile from '../components/UserProfile';
-import { Container } from '@mui/material';
 
 const ProfilePage = () => {
     return (
-        <Container>
-            <UserProfile />
-        </Container>
+        <UserProfile />
     );
 };
 
