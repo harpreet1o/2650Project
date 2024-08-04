557d5d7e29ef8cdae1c0b72db3a7029c88da9e30
sukhpreetsidhuhere <148533328+sukhpreetsidhuhere@users.noreply.github.com>
2024-08-03 19:28:24 -0700

    Update ProfilePage.jsx

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import UserProfile from '../components/UserProfile';
-import { Container } from '@mui/material';
 
 const ProfilePage = () => {
     return (
-        <Container>
+        
             <UserProfile />
-        </Container>
+        
     );
 };
 
